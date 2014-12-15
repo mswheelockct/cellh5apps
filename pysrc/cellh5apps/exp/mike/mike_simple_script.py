d302e4b3107e3bf9c7b788eedb93bfc266f17415
Christoph Sommer <christoph.sommer@imba.oeaw.ac.at>
2014-12-15 18:02:29 +0100

    some docu

--- pysrc/cellh5apps/exp/mike/mike_simple_script.py
+++ pysrc/cellh5apps/exp/mike/mike_simple_script.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     mapping_files = {"New Folder With Items": "New Folder With Items.txt"}
     
     # this constructs your class as defined above, with a name == "my_first_test" and the ch5 and mapping files per plate
-    ma = cellh5.CH5FateAnalysis("my_first_test", cellh5_files=cellh5_files, mapping_files=mapping_files)
+    ma = cellh5.CH5FateAnalysis("output", cellh5_files=cellh5_files, mapping_files=mapping_files)
     
     # due to copying / preprocessing the time information in your files is not correct, so I set manually a time lapse for this plate of 3 min
     ma.time_lapse["New Folder With Items"] = 3
@@ -19,32 +19,37 @@ if __name__ == "__main__":
     # we also want to consider events, which occured before frame 100 -> before 300 min
     ma.read_events(onset_frame=5)
     
+    # these events are still fixed window sized events (as found by CecogAnalyzer), but we can start from here and extend the tracking
+    ma.track_events()
+    
+    
+    # set transmat
     transmat = numpy.array([
                             [100,  0,   1], # "Inter" (inter can go back to mito)
                             [  0,  1,   0], # "Death" (dead is dead)
                             [  1,  0, 100], # "Mito"  (mito can go back to inter)
                           ])
     transmat = transmat.astype(numpy.float64)
-
-    # these events are still fixed window sized events (as found by CecogAnalyzer), but we can start from here and extend the tracking
-    ma.track_events()
-    
     ma.setup_hmm(transmat, "hmm_config_3c.xml")
+    
+    # predict with hmm
     ma.predict_hmm()
     
-    # for some reason your classification has weird labels not 1,2,3 as usual, but 1,4,10. I would change this in CecogAnalyzer
+    # get some overview as output as txt
     print "Overview:"
     print ma
     
+    # get uncorrected tracks as txt
     print "Uncorrected tracks"
     ma.print_tracks("Track Labels")
     
+    # get corrected tracks as txt
     print "Corrected tracks:"
     ma.print_tracks("HMM Track Labels")
     
+    # show a specific track visually
     ma.show_track(43)
     
-    print "Fini"
         
         
 
